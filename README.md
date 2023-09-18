6156fd73bf899df91c76031731755489b6142d6e
Ricardo França <ricardofs547@live.com>
2023-09-18 19:47:44 -0300

    add README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Adivinhai o número
+Atividade para a caderia de programação para dispositivos móveis
---
+++ app/src/main/java/br/edu/ifpb/acerteonumero/Venceu.kt
@@ -0,0 +1,11 @@
+package br.edu.ifpb.acerteonumero
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Venceu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_venceu)
+    }
+}
